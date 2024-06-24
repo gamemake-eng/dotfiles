00e093ae61014c392606efb66d61911ca3d7c6c2
gamemake-eng <mohinathsivaneswaran@gmail.com>
2024-06-24 17:40:57 -0400

    Added vim config

--- fetch.sh
+++ fetch.sh
@@ -1,4 +1,8 @@
+#Michealtheratz's fancy dotfile fetch script
+
 rm -r sway kitty waybar
+
+yes "yes" | rm -r vim
 
 mkdir sway
 cp -r ~/.config/sway/* sway
@@ -8,3 +12,6 @@ cp -r ~/.config/kitty/* kitty
 
 mkdir waybar
 cp -r ~/.config/waybar/* waybar
+
+mkdir vim
+cp -r ~/.vim/vimrc vim
